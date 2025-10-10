5f0d0f9ab817d2b6e61ef7e0966ff78015700304
Santiago Arquez <santiagoe-arquezc@unilibre.edu.co>
2025-10-10 20:52:38 +0000

    Actualización en codigo pero con las variables nuevas

--- Prediccion_de_donantes.py
+++ Prediccion_de_donantes.py
@@ -5,8 +5,8 @@ df=pd.read_csv("donantes.csv",sep=";")
 print("Datos cargados correctamente")
 print(df.head())
 
-def predecir_donacion(frecuencia_dias,frecuencia_12m):
-    if frecuencia_dias<180 and frecuencia_12m>=2:
+def predecir_donacion(dias_desde,frecuencia_12m):
+    if dias_desde<180 and frecuencia_12m>=2:
         print("Probablemente vuelva a donar")
     else:
         print("Probablemente no vuelva a donar")
