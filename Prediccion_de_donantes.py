ab63e4054fd412c7f0f15320213593407b3ed9a8
Santiago Arquez <santiagoe-arquezc@unilibre.edu.co>
2025-10-10 21:23:48 +0000

    Actualización del código de predicción

--- Prediccion_de_donantes.py
+++ Prediccion_de_donantes.py
@@ -5,8 +5,14 @@ df=pandas.read_csv("donantes.csv",sep=";")
 print("Datos cargados correctamente")
 print(df.head())
 
-def predecir_donacion(dias_desde,frecuencia_12m):
-    if dias_desde<180 and frecuencia_12m>=2:
-        print("Probablemente vuelva a donar")
-    else:
-        print("Probablemente no vuelva a donar")
+df["prediccion"] = df.apply(
+    lambda fila: "Probablemente volvera a donar"
+    if fila["dias_desde"]<180 and fila["frecuencia_12m"]>=2
+    else "Probablemente no volvera a donar",
+    axis=1
+)
+
+# Mostrar los resultados completos
+print("-------------RESULTADOS DE PREDICCION--------------")
+print(df[["nombre", "dias_desde", "frecuencia_12m", "volumen_total_ml", "prediccion"]])
+
