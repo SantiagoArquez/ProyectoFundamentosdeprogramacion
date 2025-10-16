1d9c9f7e220f15d84a5a9afb73c560b7f9f0c7fc
Santiago Arquez <santiagoe-arquezc@unilibre.edu.co>
2025-10-16 01:39:01 +0000

    Actualizacion del codigo para mostrar todos los datos

--- Prediccion_de_donantes.py
+++ Prediccion_de_donantes.py
@@ -1,9 +1,13 @@
 import pandas
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.max_colwidth', None)
+pandas.set_option('display.width', None) 
 
 df=pandas.read_csv("donantes.csv",sep=";")
 
 print("Datos cargados correctamente")
-print(df.head())
+print(df.to_string())
 
 df["prediccion"] = df.apply(
     lambda fila: "Probablemente volvera a donar"
@@ -12,7 +16,7 @@ df["prediccion"] = df.apply(
     axis=1
 )
 
-# Mostrar los resultados completos
+
 print("-------------RESULTADOS DE PREDICCION--------------")
 print(df[["nombre", "dias_desde", "frecuencia_12m", "volumen_total_ml", "prediccion"]])
 
